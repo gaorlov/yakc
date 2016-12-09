150402620f6187001383bb17d98485af14a015ef
Greg <greg@avvo.com>
2016-12-09 14:35:48 -0800

    starting on tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,30 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'simplecov'
+SimpleCov.start
+
 require 'yakc'
 
 require 'minitest/autorun'
+
+class TestMessage < YAKC::Message
+  attr_reader :payload
+  
+  def broadcastable?
+    event.present?
+  end
+
+  def event
+    message[:event][:name]
+  rescue
+    nil
+  end
+
+  protected
+
+  def parse( message )
+    message
+  end
+end
+
+handler = YAKC::MessageBroadcaster.new message_class: TestMessage
